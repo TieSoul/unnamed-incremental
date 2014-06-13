fc00ac38b4cc97f26924a425f908ae3a05e4bde3
Delia Eris <asphodelia.erin@gmail.com>
2014-06-13 15:52:05 -0500

    add ability to reset the game (no prestige yet)

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function Init() {  //Initializes the game
-    Game = new Object();
-    Game.points = 0;
+function Init() {  // Run each time we start up
     load();
     save_timer = setInterval(save, 30*1000); // autosave every 30 seconds
     onunload = save; // autosave when leaving the page (e.g. closing the tab, going to another page, or reloading)
+    
+    document.getElementById("click").onclick = click;
+    document.getElementById("reset").onclick = reset;
+}
+function newgame() { // Initialize a new game
+    Game = new Object();
+    Game.points = 0;
+    save();
 }
 
 function save() {
@@ -12,6 +18,8 @@ function save() {
 function load() {
     if ('Game' in localStorage) {
         Game = JSON.parse(localStorage.Game);
+    } else {
+        newgame();
     }
     display();
 }
@@ -24,8 +32,16 @@ function Beautify(what,floats) //will expand on this function later.
 {
     return what
 }
-document.getElementById("click").onclick = function() {  //Makes the button increase point amount
+
+function click() {  //Makes the button increase point amount
     Game.points++;
     display();
 }
+function reset() {
+    if (confirm("Are you sure you want to reset the game?")) {
+        newgame();
+        display();
+    }
+}
+
 Init();
