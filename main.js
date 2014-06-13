5379275aee62e05a33b2f90d51cca5b73b20d766
TieSoul <tdbaksteen@gmail.com>
2014-06-13 20:15:38 +0200

    Added beautify

--- main.js
+++ main.js
@@ -3,9 +3,22 @@ var Init = function() {  //Initializes the game
     Game.points = 0;
 }
 
+var Beautify = function(num, floats) {
+  var isFloating = (num == floor(num));
+  numstring = num + "";
+  if (isFloating) {
+    var afterFloat = numstring.substring(numstring.indexOf("."));
+    if (Boolean(afterFloat.substring(0,floats).match(/[1-9]/))) {
+      num = floor(num) + "";
+    }
+    else num = numstring.substring(0,numstring.indexOf(".")-1) + afterFloat.substring(0,floats);
+    try {var beforeFloat = num.substring(0,numstring.indexOf(".")-1);} catch Error {}
+  }
+}
+
 document.getElementById("click").onclick = function() {  //Makes the button increase point amount
   Game.points++;
   document.getElementById("count").innerHTML = Game.points; 
 }
-
+document.getElementById("count").innerHTML = Beautify(1.0000001,5);
 Init();
