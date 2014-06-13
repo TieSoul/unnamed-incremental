18b7c16f21954f91f49e7ab09981880465730bfa
Delia Eris <asphodelia.erin@gmail.com>
2014-06-13 15:11:43 -0500

    automatically save and load

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 function Init() {  //Initializes the game
-    window.Game = new Object();
+    Game = new Object();
     Game.points = 0;
+    load();
+    save_timer = setInterval(save, 30*1000); // autosave every 30 seconds
+    onunload = save; // autosave when leaving the page (e.g. closing the tab, going to another page, or reloading)
+}
+
+function save() {
+    localStorage.Game = JSON.stringify(Game);
+}
+function load() {
+    if ('Game' in localStorage) {
+        Game = JSON.parse(localStorage.Game);
+    }
+    display();
+}
+
+function display() {
+    document.getElementById("count").innerHTML = Beautify(Game.points,1);     
 }
 
 function Beautify(what,floats) //will expand on this function later.
@@ -9,7 +26,6 @@ function Beautify(what,floats) //will expand on this function later.
 }
 document.getElementById("click").onclick = function() {  //Makes the button increase point amount
     Game.points++;
-    document.getElementById("count").innerHTML = Beautify(Game.points,1); 
+    display();
 }
-
 Init();
