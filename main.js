53d09d74212b5272522b9fb078123b7e53f815b1
Delia Eris <asphodelia.erin@gmail.com>
2014-06-13 14:51:55 -0500

    clean up whitespace

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-var Init = function() {  //Initializes the game
-  window.Game = new Object();
+function Init() {  //Initializes the game
+    window.Game = new Object();
     Game.points = 0;
 }
 
 function Beautify(what,floats) //will expand on this function later.
 {
-	return what
+    return what
 }
 document.getElementById("click").onclick = function() {  //Makes the button increase point amount
-  Game.points++;
-  document.getElementById("count").innerHTML = Beautify(Game.points,1); 
+    Game.points++;
+    document.getElementById("count").innerHTML = Beautify(Game.points,1); 
 }
+
 Init();
